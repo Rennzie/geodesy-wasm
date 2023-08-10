705b778956ad0a453f355b5807759e1e2adcabd2
Sean Rennie <sean.rennie@sensat.co.uk>
2023-08-10 06:11:04 +0100

    add some errors and utils

--- src/error.rs
+++ src/error.rs
@@ -1,2 +1,9 @@
 use wasm_bindgen::JsError;
 pub type WasmResult<T> = std::result::Result<T, JsError>;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("RGError: {0}")]
+    RgError(#[from] geodesy_rs::Error),
+}
+pub type Result<T> = std::result::Result<T, Error>;
