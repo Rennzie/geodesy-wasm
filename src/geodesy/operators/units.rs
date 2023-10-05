95c1ec5addeaa13722b7c4b5068433e9ee09bca1
Sean Rennie <sean.rennie@sensat.co.uk>
2023-10-05 06:58:47 +0100

    add angular units and format to table like

--- src/geodesy/operators/units.rs
+++ src/geodesy/operators/units.rs
@@ -1,195 +1,54 @@
-// Units are taken from PROJ https://github.com/OSGeo/PROJ/blob/master/src/units.cpp
+/// Units are taken from PROJ https://github.com/OSGeo/PROJ/blob/master/src/units.c,
 
-use std::collections::HashMap;
-
-pub struct Unit {
-    pub factor: &'static str,
-    pub description: &'static str,
-    pub to_meters: f64,
+pub struct Unit(&'static str, &'static str, &'static str, f64);
+impl Unit {
+    pub fn name(&self) -> &'static str {
+        self.0
+    }
+    pub fn _factor(&self) -> &'static str {
+        self.1
+    }
+    pub fn _description(&self) -> &'static str {
+        self.2
+    }
+    pub fn multiplier(&self) -> f64 {
+        self.3
+    }
 }
 
 /// Represents a set of linear units and their conversion to meters.
-const LINEAR_UNITS: [(&str, Unit); 21] = [
-    (
-        "km",
-        Unit {
-            factor: "1000",
-            description: "Kilometer",
-            to_meters: 1000.0,
-        },
-    ),
-    (
-        "m",
-        Unit {
-            factor: "1",
-            description: "Meter",
-            to_meters: 1.0,
-        },
-    ),
-    (
-        "dm",
-        Unit {
-            factor: "1/10",
-            description: "Decimeter",
-            to_meters: 0.1,
-        },
-    ),
-    (
-        "cm",
-        Unit {
-            factor: "1/100",
-            description: "Centimeter",
-            to_meters: 0.01,
-        },
-    ),
-    (
-        "mm",
-        Unit {
-            factor: "1/1000",
-            description: "Millimeter",
-            to_meters: 0.001,
-        },
-    ),
-    (
-        "kmi",
-        Unit {
-            factor: "1852",
-            description: "International Nautical Mile",
-            to_meters: 1852.0,
-        },
-    ),
-    (
-        "in",
-        Unit {
-            factor: "0.0254",
-            description: "International Inch",
-            to_meters: 0.0254,
-        },
-    ),
-    (
-        "ft",
-        Unit {
-            factor: "0.3048",
-            description: "International Foot",
-            to_meters: 0.3048,
-        },
-    ),
-    (
-        "yd",
-        Unit {
-            factor: "0.9144",
-            description: "International Yard",
-            to_meters: 0.9144,
-        },
-    ),
-    (
-        "mi",
-        Unit {
-            factor: "1609.344",
-            description: "International Statute Mile",
-            to_meters: 1609.344,
-        },
-    ),
-    (
-        "fath",
-        Unit {
-            factor: "1.8288",
-            description: "International Fathom",
-            to_meters: 1.8288,
-        },
-    ),
-    (
-        "ch",
-        Unit {
-            factor: "20.1168",
-            description: "International Chain",
-            to_meters: 20.1168,
-        },
-    ),
-    (
-        "link",
-        Unit {
-            factor: "0.201168",
-            description: "International Link",
-            to_meters: 0.201168,
-        },
-    ),
-    (
-        "us-in",
-        Unit {
-            factor: "1/39.37",
-            description: "U.S. Surveyor's Inch",
-            to_meters: 100.0 / 3937.0,
-        },
-    ),
-    (
-        "us-ft",
-        Unit {
-            factor: "0.304800609601219",
-            description: "U.S. Surveyor's Foot",
-            to_meters: 1200.0 / 3937.0,
-        },
-    ),
-    (
-        "us-yd",
-        Unit {
-            factor: "0.914401828803658",
-            description: "U.S. Surveyor's Yard",
-            to_meters: 3600.0 / 3937.0,
-        },
-    ),
-    (
-        "us-ch",
-        Unit {
-            factor: "20.11684023368047",
-            description: "U.S. Surveyor's Chain",
-            to_meters: 79200.0 / 3937.0,
-        },
-    ),
-    (
-        "us-mi",
-        Unit {
-            factor: "1609.347218694437",
-            description: "U.S. Surveyor's Statute Mile",
-            to_meters: 6336000.0 / 3937.0,
-        },
-    ),
-    (
-        "ind-yd",
-        Unit {
-            factor: "0.91439523",
-            description: "Indian Yard",
-            to_meters: 0.91439523,
-        },
-    ),
-    (
-        "ind-ft",
-        Unit {
-            factor: "0.30479841",
-            description: "Indian Foot",
-            to_meters: 0.30479841,
-        },
-    ),
-    (
-        "ind-ch",
-        Unit {
-            factor: "20.11669506",
-            description: "Indian Chain",
-            to_meters: 20.11669506,
-        },
-    ),
+#[rustfmt::skip]
+pub const LINEAR_UNITS: [Unit; 21] = [
+    Unit("km",      "1000",              "Kilometer",                    1000.0),
+    Unit("m",       "1",                 "Meter",                        1.0),
+    Unit("dm",      "1/10",              "Decimeter",                    0.1),
+    Unit("cm",      "1/100",             "Centimeter",                   0.01),
+    Unit("mm",      "1/1000",            "Millimeter",                   0.001),
+    Unit("kmi",     "1852",              "International Nautical Mile",  1852.0),
+    Unit("in",      "0.0254",            "International Inch",           0.0254),
+    Unit("ft",      "0.3048",            "International Foot",           0.3048),
+    Unit("yd",      "0.9144",            "International Yard",           0.9144),
+    Unit("kmi",     "1609.344",          "International Statute Mile",   1609.344),
+    Unit("fath",    "1.8288",            "International Fathom",         1.8288),
+    Unit("ch",      "20.1168",           "International Chain",          20.1168),
+    Unit("link",    "0.201168",          "International Link",           0.201168),
+    Unit("us-in",   "1/39.37",           "U.S. Surveyor's Inch",         100.0 / 3937.0),
+    Unit("us-ft",   "0.304800609601219", "U.S. Surveyor's Foot",         1200.0 / 3937.0, ),
+    Unit("us-yd",   "0.914401828803658", "U.S. Surveyor's Yard",         3600.0 / 3937.0, ),
+    Unit("us-ch",   "20.11684023368047", "U.S. Surveyor's Chain",        79200.0 / 3937.0,   ),
+    Unit("us-mi",   "1609.347218694437", "U.S. Surveyor's Statute Mile", 6336000.0 / 3937.0, ),
+    Unit("ind-yd",  "0.91439523",        "Indian Yard",                  0.91439523),
+    Unit("ind-ft",  "0.30479841",        "Indian Foot",                  0.30479841),
+    Unit("ind-ch",  "20.11669506",       "Indian Chain",                 20.11669506),
 ];
 
-// Returns a map of linear units and their conversion to meters.
-// # Example
-// ```
-// use geodesy_wasm::geodesy::operators::units::linear_units_map;
-// let map = linear_units_map();
-// assert_eq!(map["km"].to_meters, 1000.0);
-// ```
-pub fn linear_units_map() -> HashMap<&'static str, &'static Unit> {
-    LINEAR_UNITS
-        .iter()
-        .map(|&(name, ref unit)| (name, unit))
-        .collect()
-}
+const GRAD_TO_RAD: f64 = 0.015707963267948967;
+const DEG_TO_RAD: f64 = 0.017453292519943296;
+
+// Angular units
+#[rustfmt::skip]
+pub const ANGULAR_UNITS: [Unit; 3] = [
+    Unit("rad",     "1.0",                  "Radian",   1.0),
+    Unit("deg",     "0.017453292519943296", "Degree",   DEG_TO_RAD),
+    Unit("grad",    "0.015707963267948967", "Grad",     GRAD_TO_RAD),
+];
