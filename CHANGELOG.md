ee737c74dace12b46b70be7e7b7becc56ccf0bd4
Sean Rennie <sean.rennie@sensat.co.uk>
2023-10-06 09:39:24 +0100

    change long things

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Security - in case of vulnerabilities.
 -->
 
+## [Unreleased]
+
+### Added
+
+- `longlat` operator and aliases [#24](https://github.com/Rennzie/geodesy-wasm/issues/24)
+
 ## [0.3.0] - 2023-10-05
 
 ### Added
--- src/geodesy/operators/mod.rs
+++ src/geodesy/operators/mod.rs
@@ -10,5 +10,6 @@ pub const ACCESSORY_OPERATORS: [(&str, OpConstructor); 2] = [
   // As far as I can tell the `longlat` operator is a no-op.
   // - https://proj.org/en/9.3/operations/conversions/latlon.html
   // - https://github.com/OSGeo/PROJ/blob/2040e685f5ab9c2958b7b611f5aaafee21fed82f/src/projections/latlong.cpp#L94
+  // My assumption is it's for historical reasons, so in geodesy-wasm it's just a noop.
   ("longlat", OpConstructor(noop::new))
 ];
